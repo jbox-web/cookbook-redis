f18150ba1ccf83086841e963c42666aad2f11f40
Nicolas Rodriguez <nicoladmin@free.fr>
2020-01-12 17:24:09 +0100

    Fix systemd config when Redis data dir is not in the default location

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,6 +19,26 @@ package 'redis-server' do
 end
 
 # Configure Redis
+if node[:redis][:config][:dir] != '/var/lib/redis'
+  directory '/etc/systemd/system/redis-server.service.d'
+
+  template '/etc/systemd/system/redis-server.service.d/data_dir.conf' do
+    source    'systemd.conf.erb'
+    variables dir: node[:redis][:config][:dir]
+  end
+
+  execute 'systemctl daemon-reload' do
+    command 'systemctl daemon-reload'
+  end
+end
+
+directory node[:redis][:config][:dir] do
+  recursive true
+  mode  '0750'
+  owner 'redis'
+  group 'redis'
+end
+
 template '/etc/redis/redis.conf' do
   source    'redis.conf.erb'
   variables config: node[:redis][:config]
